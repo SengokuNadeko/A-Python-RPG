8dfc550c68fbaaf1affc0ef17cea0b50c615edcf
JihadBeydoun <hsun25874@gmail.com>
2023-01-11 12:37:01 -0500

    Added player animation

--- sprites.py
+++ sprites.py
@@ -28,11 +28,10 @@ class Player(pygame.sprite.Sprite):
         self.x_translation = 0 #stores the player's movement on the x axis
         self.y_translation = 0 #stores the player's movement on the y axis
 
-
-
         self.player_direction = "facing_down" #stores which direction the player object is facing. it will change depending on player's movement. the default state is facing_down
+        self.image = self.game.character_sprite_sheet.get_sprite(0, 0, self.width, self.height)
 
-        self.image = self.game.character_sprite_sheet.get_sprite(0, 0, self.width, self.height) #getting the player sprite in the Arthax.png sprite sheet and storing it in the variable image.
+        self.animation_loop = 0
 
         self.rect = self.image.get_rect()
         self.rect.x = self.x
@@ -41,20 +40,20 @@ class Player(pygame.sprite.Sprite):
     def player_movement(self):
         keys = pygame.key.get_pressed()
         if(keys[pygame.K_a]):
-            self.x_translation -= MOVEMENT_SPEED
-            self.player_direction = "facing_left"
+            self.x_translation -= MOVEMENT_SPEED #moves the player object left
+            self.player_direction = "facing_left" #changes the player direction variable to the direction it's facing in.
 
         if(keys[pygame.K_d]):
-            self.x_translation += MOVEMENT_SPEED
-            self.player_direction = "facing_right"
+            self.x_translation += MOVEMENT_SPEED #moves the player object right
+            self.player_direction = "facing_right" #changes the player direction variable to the direction it's facing in.
 
         if(keys[pygame.K_w]):
-            self.y_translation -= MOVEMENT_SPEED
-            self.player_direction = "facing_up"
+            self.y_translation -= MOVEMENT_SPEED #moves the player object up
+            self.player_direction = "facing_up" #changes the player direction variable to the direction it's facing in.
 
         if(keys[pygame.K_s]):
-            self.y_translation += MOVEMENT_SPEED
-            self.player_direction = "facing_down"
+            self.y_translation += MOVEMENT_SPEED #moves the player object down
+            self.player_direction = "facing_down" #changes the player direction variable to the direction it's facing in.
 
     #defining the collision_check function to detect collision when the player object hits something that is collidable 
     def collision_check(self, direction):
@@ -73,10 +72,84 @@ class Player(pygame.sprite.Sprite):
                     self.rect.y = hits[0].rect.top - self.rect.height
                 if(self.y_translation < 0):
                     self.rect.y = hits[0].rect.bottom
+    
+    def player_animate(self):
+        #defining arrays that store multiple sprites for player animation.
+
+        #defining the walk_down_animation array and storing the sprites that contain the player moving downwards in the array
+        walk_down_animation = [
+            self.game.character_sprite_sheet.get_sprite(16, 0, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(32, 0, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(48, 0, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(64, 0, self.width, self.height)
+        ]
+
+        #defining the walk_up_animation array and storing the sprites that contain the player moving upwards in the array
+        walk_up_animation = [
+            self.game.character_sprite_sheet.get_sprite(16, 16, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(32, 16, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(48, 16, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(64, 16, self.width, self.height)
+        ]
+
+        #defining the walk_right_animation array and storing the sprites that contain the player moving right in the array
+        walk_right_animation = [
+            self.game.character_sprite_sheet.get_sprite(16, 32, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(32, 32, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(48, 32, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(64, 32, self.width, self.height)
+        ]
+
+        #defining the walk_left_animation array and storing the sprites that contain the player moving left in the array
+        walk_left_animation = [
+            self.game.character_sprite_sheet.get_sprite(16, 48, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(32, 48, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(48, 48, self.width, self.height),
+            self.game.character_sprite_sheet.get_sprite(64, 48, self.width, self.height)
+        ]
+
+        if(self.player_direction == "facing_down"):
+            if(self.y_translation == 0):
+                self.image = self.game.character_sprite_sheet.get_sprite(0, 0, self.width, self.height) #getting the player sprite in the Arthax.png sprite sheet and storing it in the variable image. this is for the downward facing position.
+            else:
+                self.image = walk_down_animation[math.floor(self.animation_loop)] #math.floor() rounds down a number to the closest whole number. here, we use animation_loop as the index to get each sprite in the animation array.
+                self.animation_loop += 0.1 #adds 0.1 to animation_loop every frame. this loops through the animation array above every time the player is, in this case, facing_down, moving, and the player_animate() function is called. 
+                if(self.animation_loop >= 4):
+                    self.animation_loop = 0 #if the animation_loop variable ends up being equal to or greater than 4, we reset it to 0. the reason we cap the maximum value at 4 is because the amount of sprites each animation array holds is 4, currently. and since the index of arrays starts at 0 instead of one, we want to cycle through 0-3 instead of 1-4. 
+        
+        if(self.player_direction == "facing_up"):
+            if(self.y_translation == 0):
+                self.image = self.game.character_sprite_sheet.get_sprite(0, 16, self.width, self.height) #getting the player sprite in the Arthax.png sprite sheet and storing it in the variable image. this is for the upward facing position.
+            else:
+                self.image = walk_up_animation[math.floor(self.animation_loop)] #math.floor() rounds down a number to the closest whole number. here, we use animation_loop as the index to get each sprite in the animation array.
+                self.animation_loop += 0.1 #adds 0.1 to animation_loop every frame. this loops through the animation array above every time the player is, in this case, facing_up, moving, and the player_animate() function is called. 
+                if(self.animation_loop >= 4):
+                    self.animation_loop = 0 #if the animation_loop variable ends up being equal to or greater than 4, we reset it to 0. the reason we cap the maximum value at 4 is because the amount of sprites each animation array holds is 4, currently. and since the index of arrays starts at 0 instead of one, we want to cycle through 0-3 instead of 1-4. 
+        
+        if(self.player_direction == "facing_right"):
+            if(self.x_translation == 0):
+                self.image = self.game.character_sprite_sheet.get_sprite(0, 32, self.width, self.height) #getting the player sprite in the Arthax.png sprite sheet and storing it in the variable image. this is for the right facing position.
+            else:
+                self.image = walk_right_animation[math.floor(self.animation_loop)] #math.floor() rounds down a number to the closest whole number. here, we use animation_loop as the index to get each sprite in the animation array.
+                self.animation_loop += 0.1 #adds 0.1 to animation_loop every frame. this loops through the animation array above every time the player is, in this case, facing_right, moving, and the player_animate() function is called. 
+                if(self.animation_loop >= 4):
+                    self.animation_loop = 0 #if the animation_loop variable ends up being equal to or greater than 4, we reset it to 0. the reason we cap the maximum value at 4 is because the amount of sprites each animation array holds is 4, currently. and since the index of arrays starts at 0 instead of one, we want to cycle through 0-3 instead of 1-4. 
+        
+        if(self.player_direction == "facing_left"):
+            if(self.x_translation == 0):
+                self.image = self.game.character_sprite_sheet.get_sprite(0, 48, self.width, self.height) #getting the player sprite in the Arthax.png sprite sheet and storing it in the variable image. this is for the left facing position.
+            else:
+                self.image = walk_left_animation[math.floor(self.animation_loop)] #math.floor() rounds down a number to the closest whole number. here, we use animation_loop as the index to get each sprite in the animation array.
+                self.animation_loop += 0.1 #adds 0.1 to animation_loop every frame. this loops through the animation array above every time the player is, in this case, facing_left, moving, and the player_animate() function is called. 
+                if(self.animation_loop >= 4):
+                    self.animation_loop = 0 #if the animation_loop variable ends up being equal to or greater than 4, we reset it to 0. the reason we cap the maximum value at 4 is because the amount of sprites each animation array holds is 4, currently. and since the index of arrays starts at 0 instead of one, we want to cycle through 0-3 instead of 1-4. 
 
     def update(self):
-        #calling the player_movement variable. we call it in update to check each frame if the player is moving.
+        #calling the player_movement function. we call it in update to check each frame if the player is moving.
         self.player_movement()
+
+        #calling the player_animate() function. we call it in update to animate the player object every frame.
+        self.player_animate()
 
         #changes the player's position on the map. we do this in the update method since it gets called each frame.
         self.rect.x += self.x_translation
